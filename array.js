c85da657d47613fb3057327f2d67d4360c0f6e10
Ayon95 <mushfiqurrahman78@yahoo.com>
2021-09-09 01:05:07 -0400

    Added code for implementing an array

--- array.js
+++ array.js
@@ -22,3 +22,56 @@ strings.unshift("x");
 // adding 'hello' at index position 2
 // O(n) because it needs to iterate over each item and assign it the right index
 strings.splice(2, 0, "hello");
+
+console.log(this);
+
+/* --------- Array implementation ----------
+
+- arrays in JS are objects with integer-based keys that act like indices
+*/
+
+class MyArray {
+  constructor(length, data) {
+    this.length = length;
+    this.data = data;
+  }
+
+  get(index) {
+    return this.data[index];
+  }
+
+  push(item) {
+    this.data[this.length] = item;
+    this.length++;
+    return this.length;
+  }
+
+  pop() {
+    const lastItem = this.data[this.length - 1];
+    delete this.data[this.length - 1];
+    this.length--;
+    return lastItem;
+  }
+
+  remove(index) {
+    // don't do anything if the array is empty or index is out of bounds
+    if (this.length === 0 || index > this.length - 1) return;
+    for (let i = index; i < this.length - 1; i++) {
+      // shifting every item to the left starting from the index of the item that we want to delete
+      // if we delete the first item (index 0), then 0 will be the index of second item (it will become the first item), and so on
+      this.data[i] = this.data[i + 1];
+    }
+    // need to delete the last item since we are not iterating over it (i < this.length - 1)
+    // we are not iterating over the last item because i + 1 will be out of bounds
+    delete this.data[this.length - 1];
+    this.length--;
+  }
+}
+
+const array1 = new MyArray(0, {});
+array1.push("how");
+array1.push("are");
+array1.push("you?");
+array1.remove(2);
+console.log(array1);
+console.log(array1.length);
